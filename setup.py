9fa033a4edf49763de2924833ecdf43b6c40b72d
Swall0w <technext.jpn@gmail.com>
2017-07-07 13:13:10 +0900

    [add]: Add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="clib",
+    version="0.1",
+    description="chainer module",
+    url="https://github.com/Swall0w/clib",
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    pig = pig.pig:main
+    """,
+    )
