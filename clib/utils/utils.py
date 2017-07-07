7a8d89fbc74d7b98f84cda02c7a41d4f17fe5ec3
Swall0w <technext.jpn@gmail.com>
2017-07-08 00:06:21 +0900

    [update]: Move print functions to debug.

--- clib/utils/utils.py
+++ clib/utils/utils.py
@@ -12,45 +12,6 @@ def reorg(input, stride=2):
     output = F.reshape(output, (batch_size, output_channel, output_height, output_width))
     return output
 
-
-def print_cnn_info(name, link, shape_before, shape_after, time):
-    n_stride = (
-        int((shape_before[2] + link.pad[0] * 2 - link.ksize) / link.stride[0]) + 1,
-        int((shape_before[3] + link.pad[1] * 2 - link.ksize) / link.stride[1]) + 1
-    )
-
-    cost = n_stride[0] * n_stride[1] * shape_before[1] * link.ksize * link.ksize * link.out_channels
-
-    print('%s(%d × %d, stride=%d, pad=%d) (%d x %d x %d) -> (%d x %d x %d) (cost=%d): %.6f[sec]' % 
-        (
-            name, link.W.shape[2], link.W.shape[3], link.stride[0], link.pad[0],
-            shape_before[2], shape_before[3], shape_before[1], shape_after[2], shape_after[3], shape_after[1],
-            cost, time
-        )
-    )
-
-    return cost
-
-def print_pooling_info(name, filter_size, stride, pad, shape_before, shape_after, time):
-    n_stride = (
-        int((shape_before[2] - filter_size) / stride) + 1,
-        int((shape_before[3] - filter_size) / stride) + 1
-    )
-    cost = n_stride[0] * n_stride[1] * shape_before[1] * filter_size * filter_size * shape_after[1]
-
-    print('%s(%d × %d, stride=%d, pad=%d) (%d x %d x %d) -> (%d x %d x %d) (cost=%d): %.6f[sec]' % 
-        (name, filter_size, filter_size, stride, pad, shape_before[2], shape_before[3], shape_before[1], shape_after[2], shape_after[3], shape_after[1], cost, time)
-    )
-
-    return cost
-
-def print_fc_info(name, link, time):
-    import pdb
-    cost = link.W.shape[0] * link.W.shape[1]
-    print('%s %d -> %d (cost = %d): %.6f[sec]' % (name, link.W.shape[1], link.W.shape[0], cost, time))
-
-    return cost
-
 # x, y, w, hの4パラメータを保持するだけのクラス
 class Box():
     def __init__(self, x, y, w, h):
@@ -91,9 +52,6 @@ class Box():
         self.x = (right + left) / 2
         self.y = (bottom + top) / 2
         return self
-
-class IMGID(Box):
-    pass
 
 # 2本の線の情報を受取り、被ってる線分の長さを返す。あくまで線分
 def overlap(x1, len1, x2, len2):
