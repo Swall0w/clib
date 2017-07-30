cfd89479167f71de8f7f2fb7569679d39cb35884
Swall0w <technext.jpn@gmail.com>
2017-07-30 23:54:43 +0900

    [add]: Add Self-Organizing Maps.

---
+++ examples/som/train.py
@@ -0,0 +1,14 @@
+import numpy as np
+import chainer
+import chainer.links as L
+from chainer import Chain, Variable
+from chainer import datasets
+
+class SOM(chain):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
