46f367c30108c3f22167e1f145b5cc0aa5408d6a
Swall0w <technext.jpn@gmail.com>
2017-08-18 23:39:15 +0900

    [add]: Add docstring.

--- clib/tmp.py
+++ clib/tmp.py
@@ -1,3 +1,10 @@
 def sumfunc(x):
+    """
+    Incremental function
+
+    This function increments input value.
+    It returns the value that the input value added one.
+    """
+
     x = x + 1
     return x
