61a1c1044a12924643ac077514fa7b68940b624b
Swall0w <technext.jpn@gmail.com>
2017-08-18 21:11:34 +0900

    [add]: Add test dirs.

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from setuptools import setup, find_packages
 
-setup(name="clib",
-    version="0.15",
-    description="chainer module",
+setup(
+    name="clib",
+    version="0.16",
+    description="Neural Network module with chainer",
     url="https://github.com/Swall0w/clib",
-    packages=find_packages(),
+    install_requires=['numpy','chainer'],
+    license=license,
+    packages=find_packages(exclude=('tests')),
+    test_suite='tests',
     entry_points="""
     [console_scripts]
     pig = pig.pig:main
