fa536c3fe58dce24cff513208653e3e71e9c1cb5
Swall0w <technext.jpn@gmail.com>
2017-08-18 22:55:06 +0900

    [add]: Add finerure script.

---
+++ examples/recognition/finetune.py
@@ -0,0 +1,13 @@
+from chainer.links import VGG16Layers
+from PIL import Image
+
+def main():
+    model = VGG16Layers()
+    img = Image.open('0.jpg')
+    feature = model.extract([img], layers=['fc7'])['fc7']
+    print(feature)
+    print(len(feature[0]))
+
+
+if __name__ == '__main__':
+    main()
