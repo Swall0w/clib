f80ebfc7db0ca4a1085767ebcb5a8de459f44dc5
Swall0w <technext.jpn@gmail.com>
2017-08-19 17:02:43 +0900

    [add]: Add testcode vizualization bbox.

---
+++ tests/utils/test_visualize_bbox.py
@@ -0,0 +1,25 @@
+import unittest
+
+import clib
+#from clib.utils.visualize_bbox import viz_bbox
+from clib.utils import viz_bbox
+import os
+
+from PIL import Image
+
+
+class VisBboxTest(unittest.TestCase):
+    def setUp(self):
+        self.img = Image.open(os.path.abspath('./tests/data/0.jpg'))
+        self.output = [{"class": "person",
+                        "left": 174,
+                        "right": 349,
+                        "top": 101,
+                        "bottom": 351,
+                        "prob": 1}]
+
+    def test_vi_bbox(self):
+        self.assertIsInstance(viz_bbox(self.img, self.output), Image.Image)
+
+if __name__ == '__main__':
+    unittest.main()
