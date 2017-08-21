ba83ffcd488aa3e5bf4cbfd210ed35baf22f5996
Swall0w <technext.jpn@gmail.com>
2017-08-21 23:51:46 +0900

    [update]: Fix mutual import

--- clib/training/__init__.py
+++ clib/training/__init__.py
@@ -1,6 +1,3 @@
-from clib.training.dataset.yolo import YoloPreprocessedDataset
-from clib.training.iterator.yolo import YoloIterator
-from clib.training.updater.yolo import (darknet_converter,
-                                        yolo_converter,
-                                        YoloUpdater)
-from clib.training.dataset.unifieddataset import UnifiedLabeledImageDataset
+from clib.training import dataset
+from clib.training import iterator
+from clib.training import updater
--- clib/training/dataset/__init__.py
+++ clib/training/dataset/__init__.py
@@ -1,2 +1,5 @@
+from clib.training.dataset import yolo
+from clib.training.dataset import unifieddataset
+
 from clib.training.dataset.yolo import YoloPreprocessedDataset
 from clib.training.dataset.unifieddataset import UnifiedLabeledImageDataset
