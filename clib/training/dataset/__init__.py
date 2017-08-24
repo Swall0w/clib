52c1d462d216c7ec5f4a1d7fbbff463b9ce9d5da
Swall0w <technext.jpn@gmail.com>
2017-08-24 23:25:24 +0900

    [update]: Test pass.

--- clib/training/dataset/__init__.py
+++ clib/training/dataset/__init__.py
@@ -4,4 +4,4 @@ from clib.training.dataset import xmldataset
 
 from clib.training.dataset.yolo import YoloPreprocessedDataset
 from clib.training.dataset.unifieddataset import UnifiedLabeledImageDataset
-from clib.training.dataset.unifieddataset import XMLLabeledImageDataset
+from clib.training.dataset.xmldataset import XMLLabeledImageDataset
