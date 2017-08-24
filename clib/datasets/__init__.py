bd3ad440ef711b909145f3926ad10ef3acdcee6c
Swall0w <technext.jpn@gmail.com>
2017-08-24 23:21:00 +0900

    [add]: load func in voc xml format.

---
+++ clib/datasets/__init__.py
@@ -0,0 +1,3 @@
+from clib.datasets import voc_xmlload
+
+from clib.datasets.voc_xmlload import voc_load
---
+++ clib/datasets/voc_xmlload.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+
+
+def voc_load(file_path):
+    with open(file_path) as f:
+        tree = ET.parse(f)
+        xmlroot = tree.getroot()
+        size = xmlroot.find('size')
+        width = int(size.find('width').text)
+        height = int(size.find('height').text)
+
+        ground_truths = []
+        for obj in xmlroot.iter('object'):
+            xmlbox = obj.find('bndbox')
+            ground_truths.append({
+                'xmin': int(xmlbox.find('xmin').text),
+                'ymin': int(xmlbox.find('ymin').text),
+                'xmax': int(xmlbox.find('xmax').text),
+                'ymax': int(xmlbox.find('ymax').text),
+                'label': obj.find('name').text
+            })
+    return (width, height), ground_truths
