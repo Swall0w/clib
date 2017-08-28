3fce6d740ece850f486111948a45dafadd75876b
Swall0w <technext.jpn@gmail.com>
2017-08-28 23:09:20 +0900

    [update]: Use default chainer implemented.

--- clib/training/dataset/yolo.py
+++ clib/training/dataset/yolo.py
@@ -2,55 +2,14 @@
 import glob
 import os
 import random
+import chainer
 
 import numpy as np
 
 import cv2
 
 
-class DatasetMixin(object):
-    """Default implementation of dataset indexing.
-    DatasetMixin provides the :meth:`__getitem__` operator. The default
-    implementation uses :meth:`get_example` to extract each example, and
-    combines the results into a list. This mixin makes it easy to implement a
-    new dataset that does not support efficient slicing.
-
-    Dataset implementation using DatasetMixin still has to provide the
-    :meth:`__len__` operator explicitly.
-
-    """
-
-    def __getitem__(self, index):
-        """Returns an example or a sequence of examples.
-
-        It implements the standard Python indexing. It uses the
-        :meth:`get_example` method by default, but it may be overridden by the
-        implementation to, for example, improve the slicing performance.
-
-        """
-        if isinstance(index, slice):
-            current, stop, step = index.indices(len(self))
-            ret = []
-            while current < stop and step > 0 or current > stop and step < 0:
-                ret.append(self.get_example(current))
-                current += step
-            return ret
-        else:
-            return self.get_example(index)
-
-    def __len__(self):
-        """Returns the number of data points."""
-        raise NotImplementedError
-
-    def get_example(self, i):
-        """Returns the i-th example.
-        Implementations should override it. It should raise :class:`IndexError`
-        """
-        raise NotImplementedError
-
-
-# class PreprocessedDataset(chainer.dataset.DatasetMixin):
-class YoloPreprocessedDataset(DatasetMixin):
+class YoloPreprocessedDataset(chainer.dataset.DatasetMixin):
     def __init__(self, dirs=('JPEGImages/', 'labels/'), root='data/',
                  resize=224, tags='voc.names', random=True):
         self.root = root
