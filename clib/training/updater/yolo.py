4fc392f81ec8a24213c0d27e6ef61bd3308180f5
Swall0w <technext.jpn@gmail.com>
2017-08-30 06:12:26 +0900

    [Remove]: default converter.

--- clib/training/updater/yolo.py
+++ clib/training/updater/yolo.py
@@ -70,67 +70,6 @@ def yolo_converter(batch, device=None):
         return x, label
 
 
-class DarknetUpdater(training.StandardUpdater):
-    def __init__(self, iterator, optimizer, device=None, loss_func=None):
-        if isinstance(iterator, itr_module.Iterator):
-            iterator = {'main': iterator}
-
-        if not isinstance(optimizer, dict):
-            optimizer = {'main': optimizer}
-
-        self._iterators = iterator
-        self._optimizers = optimizer
-        self.device = device
-        self.iteration = 0
-        self.loss_func = loss_func
-
-    def update_core(self):
-        optimizer = self._optimizers['main']
-
-        batch = self._iterators['main'].next()
-        in_arrays = self.converter(batch, self.device)
-        loss_func = self.loss_func or optimizer.target
-
-        if isinstance(in_arrays, tuple):
-            optimizer.update(loss_func, *in_arrays)
-        elif isinstance(in_arrays, dict):
-            optimizer.update(loss_func, **in_arrays)
-        else:
-            optimizer.update(loss_func, in_arrays)
-
-    def converter(self, batch, device=None):
-        from chainer import cuda
-        import numpy
-
-        def _concat_arrays(arrays):
-            if (not isinstance(arrays[0], numpy.ndarray) and
-                    not isinstance(arrays[0], cuda.ndarray)):
-                arrays = numpy.asarray(arrays)
-            xp = cuda.get_array_module(arrays[0])
-            with cuda.get_device(arrays[0]):
-                return xp.concatenate([array[None] for array in arrays])
-
-        def _to_device(device, x):
-            if device is None:
-                return x
-            elif device < 0:
-                return cuda.to_cpu(x)
-            else:
-                return cuda.to_gpu(x, device, cuda.Stream.null)
-        if len(batch) == 0:
-            raise ValueError('batch is empty')
-
-        first_elem = batch[0]
-        if isinstance(first_elem, tuple):
-            x = _to_device(device, _concat_arrays([item[0] for item in batch]))
-            x = Variable(x)
-            label = [item[1][0]['one_hot_label'] for item in batch]
-            label = Variable(np.array(label, dtype=np.float32))
-            if device >= 0:
-                label.to_gpu()
-            return x, label
-
-
 class YoloUpdater(training.StandardUpdater):
     def __init__(self, iterator, optimizer, converter=yolo_converter,
                  device=None, loss_func=None):
