d8fdb07e54a03830ab559e5ce37f902dddc4f5af
Swall0w <technext.jpn@gmail.com>
2017-09-03 09:54:25 +0900

    [add]: ImageInference class.

---
+++ clib/utils/inference.py
@@ -0,0 +1,34 @@
+from clib.utils import load_class
+from chainer import serializers, cuda
+from PIL import Image
+import numpy as np
+
+
+class ImageInference(object):
+    def __init__(self)
+        self.weights = weights
+        self.labelfile = labelfile
+        self.gpu = gpu
+        self.model = model
+
+        self.label = load_class(self.labelfile)
+        serializers.load_npz(self.weights, self.model)
+
+        if self.gpu >= 0:
+            cuda.get_device(self.gpu).use()
+            self.model.to_gpu()
+
+    def load_image(self, imagefile):
+        img = Image.open(imagefile)
+        cv_img = np.array(imagefile)[:, :, ::-1].copy()
+        return cv_img
+
+    def __call__(self, inputs):
+        """Applies inference computation to input arrays.
+    
+        Args:
+            inputs: input arrays.
+        Return:
+            dictionary: Dictionary of :class: objects.
+        """
+        raise NotImplementedError()
