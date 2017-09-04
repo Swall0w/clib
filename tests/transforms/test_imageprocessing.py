fec28371f4c869b52e8b860d6a240c7fb35d412a
Swall0w <technext.jpn@gmail.com>
2017-09-04 16:56:53 +0900

    [add]: test code for elastic transform.

---
+++ tests/transforms/test_imageprocessing.py
@@ -0,0 +1,12 @@
+import unittest
+from clib.transforms import elastic_transform
+from skimage import io, data
+
+class ElasticTransformTest(unittest.TestCase):
+    def setUp(self):
+        self.grayimg = data.coins()
+        self.rgbimg = data.astronaut()
+
+    def test_elastic_transform(self):
+        self.assertEqual(elastic_transform(self.grayimg).shape, (303, 384))
+        self.assertEqual(elastic_transform(self.rgbimg).shape, (512, 512, 3))
