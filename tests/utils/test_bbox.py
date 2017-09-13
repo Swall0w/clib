1b435fd21315e6d32c40f6d5a0d2e2da7a5d9317
Swall0w <technext.jpn@gmail.com>
2017-09-13 17:22:12 +0900

    [update]: test code of bbox.

--- tests/utils/test_bbox.py
+++ tests/utils/test_bbox.py
@@ -12,6 +12,10 @@ class BboxTest(unittest.TestCase):
     def test_vi_bbox(self):
         self.assertEqual(self.bbox.int_left_top(), (30, 20))
         self.assertEqual(self.bbox.int_right_bottom(), (70, 80))
+        self.assertEqual(self.bbox.left_top(), [30.0, 20.0])
+        self.assertEqual(self.bbox.right_bottom(), [70.0, 80.0])
+        self.bbox.crop_region(5, 5)
+        self.assertEqual(self.bbox.right_bottom(), [5.0, 5.0])
 
 if __name__ == '__main__':
     unittest.main()
