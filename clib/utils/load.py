afe2ba936e887e7bd1b302fe8b877c846cf8b8cb
Swall0w <technext.jpn@gmail.com>
2017-09-15 11:57:50 +0900

    [update]: fix load tags.

--- clib/utils/load.py
+++ clib/utils/load.py
@@ -1,4 +1,4 @@
 def load_class(namesfile):
     with open(namesfile, 'r') as f:
-        tags = {item.strip(): int(x) for x, item in enumerate(f.readlines())}
+        tags = {int(x): item.strip() for x, item in enumerate(f.readlines())}
     return tags
