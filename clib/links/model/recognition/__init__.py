fea44140be639f1090396b5f3f1b53f2c824f227
Swall0w <technext.jpn@gmail.com>
2017-09-22 23:11:23 +0900

    [update]: init.

--- clib/links/model/recognition/__init__.py
+++ clib/links/model/recognition/__init__.py
@@ -1,6 +1,7 @@
 from clib.links.model.recognition import alexnet
 from clib.links.model.recognition import densenet
 from clib.links.model.recognition import squeezenet
+from clib.links.model.recognition import res
 
 from clib.links.model.recognition.alexnet import Alex
 from clib.links.model.recognition.densenet import (DenseBlock,
@@ -8,3 +9,5 @@ from clib.links.model.recognition.densenet import (DenseBlock,
                                                    DenseNet
                                                    )
 from clib.links.model.recognition.squeezenet import Fire, SqueezeNet
+from clib.links.model.recognition.res import (BottleNeckA, BottleNeckB,
+                                              ResNet50, ResNet101, ResNet152)
