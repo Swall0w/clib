c699fc74b5ac851b7e2e1c24927f712eaefe52f1
Swall0w <technext.jpn@gmail.com>
2018-05-27 06:03:30 +0000

    [update]: easier to break the loop.

--- clib/transforms/coordinate.py
+++ clib/transforms/coordinate.py
@@ -5,22 +5,24 @@ def jitter_position(bbox, size, step=(0, 0)):
 
     while True:
         x_min_step = random.randint(-step[0], step[0])
-        y_min_step = random.randint(-step[1], step[1])
         x_max_step = random.randint(-step[0], step[0])
-        y_max_step = random.randint(-step[1], step[1])
-
         new_x_min = bbox[0] - x_min_step
-        new_y_min = bbox[1] - y_min_step
         new_x_max = bbox[2] - x_max_step
-        new_y_max = bbox[3] - y_max_step
-
-        new_width =  new_x_max - new_x_min
-        new_height =  new_y_max - new_y_min
-
-        new_bbox = (new_x_min, new_y_min, new_x_max, new_y_max)
-
+        new_bbox = (new_x_min, bbox[1], new_x_max, bbox[3])
         if (_check_position(new_bbox, size)):
             break
+
+    while True:
+        y_min_step = random.randint(-step[1], step[1])
+        y_max_step = random.randint(-step[1], step[1])
+        new_y_min = bbox[1] - y_min_step
+        new_y_max = bbox[3] - y_max_step
+        new_bbox = (new_bbox[0], new_y_min, new_bbox[2], new_y_max)
+        if (_check_position(new_bbox, size)):
+            break
+
+
+
     return new_bbox
 
 def _check_position(bbox, size):
